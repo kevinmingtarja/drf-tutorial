15e50e9e39ebe9da5803b5f60e4bf6cec98c8bf7
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-09-09 16:49:19 +0800

    Create Student model

--- tutorial/student/models.py
+++ tutorial/student/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    nusnet_id = models.CharField(primary_key=True, max_length=16, blank=False, unique=True)
+    name = models.CharField(max_length=128, blank=False)
+    year = models.PositiveIntegerField(blank=False)

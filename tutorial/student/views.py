8fcfd4234437afbbeeba1fac72255df78a2839fe
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-09-09 23:27:54 +0800

    Modify StudentList to use Generics

--- tutorial/student/views.py
+++ tutorial/student/views.py
@@ -1,6 +1,6 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework import status
 
 from student.models import Student
@@ -38,18 +38,6 @@ def student_list(request):
 	serializer = StudentSerializer(queryset, many=True)
 	return Response(serializer.data)
 
-class StudentList(APIView):
-	"""
-	List all students, or create a new student.
-	"""
-	def get(self, request, format=None):
-		queryset = Student.objects.all()
-		serializer = StudentSerializer(queryset, many=True)
-		return Response(serializer.data)
-	
-	def post(self, request, format=None):
-		serializer = StudentSerializer(data=request.data)
-		if serializer.is_valid():
-			serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class StudentList(generics.ListCreateAPIView):
+	queryset = Student.objects.all()
+	serializer_class = StudentSerializer

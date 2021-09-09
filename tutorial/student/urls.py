0646f9663af29144b958d5aa7bb35e07d84b740d
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-09-09 17:39:11 +0800

    Add student-detail views, register Student model to admin site

--- tutorial/student/urls.py
+++ tutorial/student/urls.py
@@ -4,4 +4,6 @@ from student import views
 urlpatterns = [
   path('hello-world', views.hello_world),
   path('hello-world-2', views.hello_world_v2),
+  path('student-manual/<str:id>', views.student_detail_manual),
+  path('student/<str:id>', views.student_detail_serializer),
 ]
--- tutorial/student/views.py
+++ tutorial/student/views.py
@@ -1,6 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from student.models import Student
+from student.serializers import StudentSerializer
+
 @api_view()
 def hello_world(request):
     return Response({"message": "Hello, world!"})
@@ -10,3 +13,19 @@ def hello_world_v2(request):
     if request.method == 'POST':
         return Response({"message": "Got some data!", "data": request.data})
     return Response({"message": "Hello, world!"})
+
+@api_view(['GET'])
+def student_detail_manual(request, id):
+	student = Student.objects.get(nusnet_id = id)
+
+	return Response({
+		"nusnet_id": student.nusnet_id,
+		"name": student.name,
+		"year": student.year
+	})
+
+@api_view(['GET'])
+def student_detail_serializer(request, id):
+	student = Student.objects.get(nusnet_id = id)
+	serializer = StudentSerializer(student)
+	return Response(serializer.data)

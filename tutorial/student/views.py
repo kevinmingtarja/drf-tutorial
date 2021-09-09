af2c900a9184eb9cf3f839db66f95e89ba56ba21
Kevin Mingtarja <kevin.mingtarja@u.nus.edu>
2021-09-09 23:59:23 +0800

    Add comments to student views

--- tutorial/student/views.py
+++ tutorial/student/views.py
@@ -6,16 +6,18 @@ from rest_framework import status
 from student.models import Student
 from student.serializers import StudentSerializer
 
-@api_view()
+
+@api_view() # Defaults to GET request since we don't specify anything
 def hello_world(request):
     return Response({"message": "Hello, world!"})
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST']) # Able to process GET and POST request
 def hello_world_v2(request):
     if request.method == 'POST':
         return Response({"message": "Got some data!", "data": request.data})
     return Response({"message": "Hello, world!"})
 
+# Less preferred compared to student_detail_serializer. Because why do it manually if we can do it automatically.
 @api_view(['GET'])
 def student_detail_manual(request, id):
 	student = Student.objects.get(nusnet_id = id)
@@ -26,6 +28,7 @@ def student_detail_manual(request, id):
 		"year": student.year
 	})
 
+# More preferred way
 @api_view(['GET'])
 def student_detail_serializer(request, id):
 	student = Student.objects.get(nusnet_id = id)
@@ -38,6 +41,7 @@ def student_list(request):
 	serializer = StudentSerializer(queryset, many=True)
 	return Response(serializer.data)
 
+# Uses Generics to simplify our code, while having the same functionalities. 
 class StudentList(generics.ListCreateAPIView):
 	queryset = Student.objects.all()
 	serializer_class = StudentSerializer
